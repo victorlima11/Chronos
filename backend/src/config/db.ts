7ed2821be72e6e59807a22b831da8c3d67be7931
victorlima11 <victorconta1009@gmail.com>
2025-08-05 10:24:22 -0300

    setup docker-compose

---
+++ backend/src/config/db.ts
@@ -0,0 +1,17 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Não foi possível encontrar a variável de ambiente DATABASE_URL.');
+}
+
+export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
+
+db.connect()
+  .then(() => console.log('Conectado ao PostgreSQL com sucesso'))
+  .catch((err) => console.error('Erro ao conectar ao PostgreSQL:', err));
