4a5e0f312aa98b937940121fc9140298df60bada
victorlima11 <victorconta1009@gmail.com>
2025-08-07 10:39:11 -0300

    add email verification

--- backend/src/middlewares/usuarioMiddleware.ts
+++ backend/src/middlewares/usuarioMiddleware.ts
@@ -1,5 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
+function validateEmail (email: string): boolean {
+    const testRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return testRegex.test(email)
+};
+
 export function validateUserRegister(req: Request, res: Response, next: NextFunction) {
 
     if (!req.body) {
@@ -7,6 +12,10 @@ export function validateUserRegister(req: Request, res: Response, next: NextFunc
     }
 
     const { nome, email, senha } = req.body;
+
+    if (!validateEmail(email)){
+        return res.status(400).json({ error: 'E-mail inválido.' });
+    }
 
     if (!nome || !email || !senha) {
         return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
